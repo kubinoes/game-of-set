8d0b7916e8d9218831415f7754c96188c9b1df7d
kubinoes <j.schovanec@protonmail.com>
2022-06-22 19:12:45 +0200

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Jakub Schovanec on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
